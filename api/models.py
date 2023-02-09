11b10b7d2515f4c6d07dfe85513225a608d558cd
Marvin <taipe.marvin17@gmail.com>
2023-02-08 21:43:30 -0500

    creando modelos Pokemon y Pokedex

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.fields import CharField, URLField,IntegerField
 
-# Create your models here.
+class Pokemon(models.Model): 
+    identifier      = CharField(max_length=100)
+    species_id      = IntegerField() 
+    height          = IntegerField() 
+    weight          = IntegerField() 
+    base_experience = IntegerField() 
+    order           = IntegerField()
+    is_default      = IntegerField()  
+
+class Pokedex(models.Model): 
+    region_id       = IntegerField() 
+    identifier      = CharField(max_length=100)
+    is_main_series  = IntegerField() 
