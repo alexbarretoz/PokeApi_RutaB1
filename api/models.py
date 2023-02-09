3cc06c79ccdb6b419e5de31a2639eb45e9445df8
Marvin <taipe.marvin17@gmail.com>
2023-02-08 21:56:33 -0500

    models vacio

--- api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Pokedex(models.Model): 
-    region_id       = IntegerField() 
-    identifier      = CharField(max_length=100)
-    is_main_series  = IntegerField() 
